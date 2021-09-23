b0d54447a7efb27da688c7650e350e0f143b1cc8
ashalkevich <alenashalkevich@gmail.com>
2021-09-23 17:53:06 +0300

    My third home task added

--- src/main/java/com/exadel/homework3/parserFromProperty/ParserTest.java
+++ src/main/java/com/exadel/homework3/parserFromProperty/ParserTest.java
@@ -6,8 +6,29 @@ public class ParserTest {
 
     public static void main(String[] args) {
 
-        HashMap<String, String> dataMap = (HashMap<String, String>) EnvironmentProperty.environmentData("environment.properties");
+//        HashMap<String, String> dataMap = (HashMap<String, String>) EnvironmentProperty.environmentData("environment.properties");
+//
+//        try {
+//            String buildEnv = dataMap.get("buildEnv");
+//            String browser = dataMap.get("browser");
+//            String testEnv = dataMap.get("testEnv");
+//
+//            if (buildEnv != null && browser != null && testEnv != null) {
+//
+//                System.out.println("Переменная testEnv: " + buildEnv);
+//                System.out.println("Переменная browser: " + browser);
+//                System.out.println("Переменная testEnv: " + testEnv);
+//
+//            } else {
+//
+//                throw new KeyNotFoundException("Переменная(-ые): " + (buildEnv == null ? "buildEnv" : "") + (browser == null ? "browser" : "") + (testEnv == null ? "testEnv" : "") + " не найдена(-ы)");
+//
+//            }
+//        } catch (KeyNotFoundException e) {
+//            System.out.println(e.getMessage());
+//        }
 
+        HashMap<String, String> dataMap = (HashMap<String, String>) ReadFile.environmentData("src/main/resources/environment.properties");
         try {
             String buildEnv = dataMap.get("buildEnv");
             String browser = dataMap.get("browser");
@@ -21,11 +42,12 @@ public class ParserTest {
 
             } else {
 
-                throw new KeyNotFoundException("Переменная(-ые): " + (buildEnv == null ? "buildEnv" : "") + (browser == null ? "browser" : "") + (testEnv == null ? "testEnv" : "") + " не найдена(-ы)");
+                throw new KeyNotFoundException("Переменная(-ые): " + (buildEnv == null ? "buildEnv " : "") + (browser == null ? "browser " : "") + (testEnv == null ? "testEnv" : "") + " не найдена(-ы)");
 
             }
         } catch (KeyNotFoundException e) {
             System.out.println(e.getMessage());
         }
+
     }
 }
---
+++ src/main/java/com/exadel/homework3/parserFromProperty/ReadFile.java
@@ -0,0 +1,29 @@
+package com.exadel.homework3.parserFromProperty;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReadFile {
+
+    public static Map<String, String> environmentData(String filePath) {
+
+        Map<String, String> dataMap = new HashMap<>();
+        try {
+            File myFile = new File(filePath);
+            FileReader fileReader = new FileReader(myFile);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                String[] m = line.split("=");
+                dataMap.put(m[0], m[1]);
+            }
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dataMap;
+    }
+}
